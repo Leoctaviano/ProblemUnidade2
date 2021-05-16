72cda040aa0a617724fc9eb93b80c821da478075
Leoctaviano <leticia.estat@yahoo.com>
2021-05-16 19:18:16 -0300

    Input post

--- functional_test.py
+++ functional_test.py
@@ -1,17 +1,13 @@
+import time
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-
-import unittest
 
 
 class NewVsitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Chrome("chromedriver.exe")
-
-    def tearDown(self):
-        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
@@ -20,52 +16,48 @@ class NewVsitorTest(unittest.TestCase):
         # aceita definir prioridades nas tarefas do tipo baixa, média e alta
         # Ela decide verificar a homepage
 
-        baixa = self.browser.find_element_by_xpath('//label[@for="baixa"]').text
-        self.assertIn('Low', baixa)
-        media = self.browser.find_element_by_xpath('//label[@for="media"]').text
-        self.assertIn('Average', media)
-        alta = self.browser.find_element_by_xpath('//label[@for="alta"]').text
-        self.assertIn('High', alta)
-
-# Ela percebe que o título da página e o cabeçalho mencionam
-# listas de tarefas com prioridade (priority to-do)
+        # Ela percebe que o título da página e o cabeçalho mencionam
+        # listas de tarefas com prioridade (priority to-do)
 
         self.assertIn('Priority To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Priority To-Do', header_text)
 
-# Ela é convidada a inserir um item de tarefa e a prioridade da
-# mesma imediatamente
+        # Ela é convidada a inserir um item de tarefa e a prioridade da
+        # mesma imediatamente
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-        inputbox.get_attribute('placeholder'),
-            'Enter a to-do item')
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
-# Ela digita "Comprar anzol" em uma nova caixa de texto
-# e assinala prioridade alta no campo de seleção de prioridades
+        # Ela digita "Comprar anzol" em uma nova caixa de texto
+        # e assinala prioridade alta no campo de seleção de prioridades
 
         inputbox.send_keys('Comprar anzol')
-        ckalta = self.browser.find_element_by_id('alta')
-        ckalta.click()
 
-# Quando ela tecla enter, a página é atualizada, e agora
-# a página lista "1 - Comprar anzol - prioridade alta"
-# como um item em uma lista de tarefas
+        # Quando ela tecla enter, a página é atualizada, e agora
+        # a página lista "1 - Comprar anzol - prioridade alta"
+        # como um item em uma lista de tarefas
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(15)
+        time.sleep(2)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Comprar anzol - prioridade alta' for row in rows))
+            any(row.text == '1 Comprar anzol - alta' for row in rows),
+            "New to-do item not appear in table"
+        )
 
-# Ainda continua havendo uma caixa de texto convidando-a a
-# acrescentar outro item. Ela insere "Comprar cola instantâne"
-# e assinala prioridade baixa pois ela ainda tem cola suficiente
-# por algum tempo
+        # Ainda continua havendo uma caixa de texto convidando-a a
+        # acrescentar outro item. Ela insere "Comprar cola instantâne"
+        # e assinala prioridade baixa pois ela ainda tem cola suficiente
+        # por algum tempo
 
         self.fail('Finish the test!')
+
 
 # A página é atualizada novamente e agora mostra os dois
 # itens em sua lista e as respectivas prioridades
