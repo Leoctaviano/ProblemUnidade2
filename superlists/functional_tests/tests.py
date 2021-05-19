11880daacf7d6eab8b84e738c13b9eecbb560267
Leoctaviano <leticia.estat@yahoo.com>
2021-05-19 20:51:04 -0300

    Url única

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -13,6 +13,9 @@ class NewVsitorTest(LiveServerTestCase):
         self.browser = webdriver.Chrome("C:\\Users\\letic\\PycharmProjects\\ProblemUnidade2\\superlists"
                                         "\\functional_tests\\chromedriver.exe")
 
+    def tearDown(self):
+        self.browser.quit()
+
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
         while True:
@@ -26,7 +29,7 @@ class NewVsitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
 
         # Edith ouviu falar que agora a aplicação online de lista de tarefas
@@ -73,7 +76,6 @@ class NewVsitorTest(LiveServerTestCase):
         select = Select(self.browser.find_element_by_id('input01'))
         select.select_by_visible_text('Baixa')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois
         # itens em sua lista e as respectivas prioridades
@@ -84,17 +86,26 @@ class NewVsitorTest(LiveServerTestCase):
 # Edith se pergunta se o site lembrará de sua lista. Então
 # ela nota que o site gerou um URL único para ela -- há um
 # pequeno texto explicativo para isso.
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Comprar anzol')
+        select = Select(self.browser.find_element_by_id('input01'))
+        select.select_by_visible_text('Alta')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Comprar anzol - Prioridade Alta')
 
-        edith_url = self.browser.current_url
-        edith_id = edith_url.split('/')[-1]
+        # Ela percebe que sua lista te um URL único
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
 # Ela acessa essa URL -- sua lista de tarefas continua lá.
         self.browser.quit()
         self.browser = webdriver.Chrome("C:\\Users\\letic\\PycharmProjects\\ProblemUnidade2\\superlists"
                                         "\\functional_tests\\chromedriver.exe")
 
-        self.browser.get('/'.join([self.live_server_url, edith_id]))
+        self.assertRegex(edith_list_url, '/lists/.+')
 
-        self.wait_for_row_in_list_table('1: Comprar anzol - Prioridade Alta')
-        self.wait_for_row_in_list_table('2: Comprar cola instantânea - Prioridade Baixa')
-        self.fail('Finish the test!')
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Comprar anzol - Prioridade Alta', page_text)
+        self.assertIn('Comprar cola instantânea - Prioridade Baixa', page_text)
--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 # Create your models here.
 class List(models.Model):
-	pass
+    pass
+
 
 class Item(models.Model):
     text = models.TextField(default='')
